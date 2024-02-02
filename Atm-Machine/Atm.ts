11983c24aa39d12a462ae137d4c633393d819dcd
Asad Raza <143411340+asadrazanaqv@users.noreply.github.com>
2024-02-03 03:23:34 +0500

    upd

---
+++ Atm-Machine/Atm.ts
@@ -0,0 +1,95 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+interface atmtype{
+    userID: number,
+    userPin:number,
+    typeofaccount: string,
+    FastCash:number,
+    typeoftransaction: string,
+    withdrawmethod: number
+}
+const balance = Math.floor(Math.random()*1000000);
+
+const answers:atmtype = await inquirer.prompt ([
+ 
+{
+    type: "input",
+    name: "userID",
+    message: "Kindly Enter Your ID"
+
+},
+{
+    type: "number",
+    name: "userPin",
+    message: "Kindly Enter Your PIN"
+
+},
+{
+    type: "list",
+    name: "typeofaccount",
+    choices:["Current","Saving","Balance Inquiry"],
+    message: "Kindly select your Account Type"
+},
+{
+    type: "list",
+    name: "typeoftransaction",
+    choices:["FastCash","Withdraw"],
+    message: "Kindly select your transaction",
+    when (answers) {
+        return answers.typeofaccount === "Current" || answers.typeofaccount === "Saving";
+    },  
+},
+{
+    type: "input",
+    name: "BalanceInquiry",
+    message:"Press Enter to check Your Balance",
+    when (answers) {
+        return answers.typeofaccount === "Balance Inquiry";
+    },
+},
+{
+    
+    type: "list",
+    name: "FastCash",
+    choices:[5000,10000,15000,20000],
+    message: "Kindly select Amount",
+    when (answers){
+        return answers.typeoftransaction === "FastCash";
+    },
+},
+{
+    
+    type: "number",
+    name: "withdrawmethod",
+    message: "Kindly Enter Your Amount",
+    when (answers){
+        return answers.typeoftransaction === "Withdraw";
+    },
+}
+]);
+
+// console.log(answers);
+
+if (answers.typeofaccount === "Balance Inquiry") {
+    console.log("Your balance is:", balance);
+} else if (answers.typeoftransaction === "FastCash") {
+    const selectedAmount = answers.FastCash;
+    if (balance >= selectedAmount) {
+        const remaining = balance - selectedAmount;
+        console.log("You took", selectedAmount, "from", balance);
+        console.log("Your remaining balance is:", remaining);
+    } else {
+        console.log("Insufficient Balance");
+    }
+} else if (answers.userID & answers.userPin) {
+    const inputamount = answers.withdrawmethod   
+    if (balance >= inputamount ) {
+    const remaining = balance - inputamount;
+    console.log("You took", inputamount, "from", balance);
+    console.log("Your remaining balance is:", remaining);
+    } else {
+        console.log("Insufficient Balance");
+    }
+};
---
+++ TodoList/todolist.ts
@@ -0,0 +1,54 @@
+import inquirer from "inquirer";
+
+let todos: string[] = [];
+let loop = true;
+
+
+const welcome = await inquirer.prompt ([
+{
+    type: "input",
+    name:"welcomed",
+    message: "Welcome to My first Todo list Press Enter to continue"
+}
+]);
+while (loop) {
+    const codes :{
+        TODO: string,
+        addmore: boolean,
+    }= await inquirer.prompt ([
+{
+    type: "input",
+    name: "TODO",
+    message: "What do you want to add in your todo"
+},
+{
+    type : "confirm",
+    name : "addmore",
+    message: "Do you want to add more todo?",
+    default: false
+}
+])
+
+    const {addmore, TODO} = codes;
+    // console.log(codes);
+    loop = addmore
+    if (TODO) {
+        todos.push(TODO)
+    } else {
+        console.log("Kindly add valid input");
+    }
+}
+
+
+// console.log(todos);
+
+if (todos.length >0) {
+    console.log("Your Todo list:");
+    
+    todos.forEach(todos => {
+        console.log(todos)
+    });
+} else {
+    console.log("No Todos Founds!!!");
+    
+}
