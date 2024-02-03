c25999afef87c9d53eed0eeaeecabbb1fe5f1a1d
Asad Raza <143411340+asadrazanaqv@users.noreply.github.com>
2024-02-03 05:06:16 +0500

    upd

---
+++ PROJECT 05_Word Counter/Readme.md
@@ -0,0 +1,2 @@
+# Word_Counter
+ CLI Based Word Counter
---
+++ PROJECT 05_Word Counter/index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import inquirer from "inquirer";
+chalkAnimation.rainbow('WELCOME TO ASAD PROGRAM');
+async function main() {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log(chalk.blueBright("Let's Start Word Counting:"));
+    const answers = await inquirer.prompt([
+        {
+            name: "paragraph",
+            message: "Enter Your Paragraph:",
+            type: "input"
+        }
+    ]);
+    let counting = answers.paragraph.trim().split(/\s+/);
+    console.log(`Your Paragraph word Count is ${counting.length}`);
+}
+main();
